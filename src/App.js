876b508dd08bf35d9457eabfdd62c113bea17e9c
Durgesh Giri <70639616+durgeshgiri123@users.noreply.github.com>
2023-03-01 22:53:44 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="bee74528-83e6-428d-98a4-dea5616938fc"
+        userName="Durgeshgiri"
+        userSecret="Durgesh@123"
+        />
+    );
+}
+
+export default App;
